9c34d71a0ea970d714af5c79572b369d9ed51658
Marc Brinkmann <git@marcbrinkmann.de>
2015-12-11 00:39:59 +0100

    Initial import.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    buf = open(os.path.join(os.path.dirname(__file__), fname), 'rb').read()
+    return buf.decode('utf8')
+
+
+setup(name='pyusbtin',
+      version='0.1.dev1',
+      description='Python driver for Thomas Fischl\'s USBtin',
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='https://github.com/mbr/pyusbtin',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['click', 'pyserial'],
+      entry_points={
+          'console_scripts': [
+              'pyusbtin = pyusbtin.cli:cli',
+          ],
+      },
+      classifiers=[
+          'Programming Language :: Python :: 3',
+      ])
